ab1c558233f382a63b87562c13ad770d70553095
DSAsefa <danielasefa975@gmail.com>
2024-12-06 15:42:33 -0700

    Header and Footer
    Created a generic header and footer

---
+++ app/menu/footer.js
@@ -0,0 +1,41 @@
+
+import GitHubLogo from "../images/GitHub-Logo-Blue.jpg";
+import Image from "next/image";
+
+export default function Footer(){
+
+
+    return(
+        <section className="bg-black text-white font-serif flex justify-around p-4">
+            
+            <div>
+                <div className="flex justify-between items-center">
+                    <p>License</p>      {/* replace with link to license */}
+                    <p>Copyright</p>	{/* replace with copyright? */}
+                </div>
+
+                <p>All rights Reserved</p>
+            </div>
+
+
+            <div className="flex items-center">
+                <p>www.video-game-wish-list.com</p>
+            </div>
+
+            <div>
+                <div className="flex justify-center items-center">
+                    <Image
+                        src={GitHubLogo}
+                        alt="GitHub Logo from ******"
+                        width={50}
+                        height="auto"
+                        className="rounded-md">
+                    </Image >
+                </div>
+
+                <p>Github</p>
+            </div>
+
+        </section>
+    );
+}
---
+++ app/menu/header.js
@@ -0,0 +1,51 @@
+
+import Image from "next/image";
+import Logo from "../images/Wishlist-WebSite-Logo.png";
+import CheckMark from "../images/CheckMark.png";
+
+export default function Header(){
+    
+    // UserInfo() function
+    // - If signed out return blank box
+    // - If signed in return box with (email, name, icon) With sign out button
+
+
+    return(
+        <section className="flex justify-around bg-black p-3  pt-4 border-b-4 mb-0  border-green-500">
+            
+            {/* Header-Left-Section = TV screen image */}
+            <Image
+                src={Logo}
+                alt="VideoGame WishList Logo made using Canva.com"
+                width={175}
+                height="auto"
+                className="rounded-md">
+            </Image >
+
+            {/* Header-Center-Section = Website Title */}
+            <section className="flex items-center">
+                <h1 className="text-3xl font-serif text-white pr-2">
+                    VideoGame
+                </h1>
+                <Image
+                    src={CheckMark}
+                    alt="Check Mark Image found on CleanPNG"
+                    // https://www.cleanpng.com/png-check-mark-royalty-free-stock-photography-clip-art-803672/download-png.html
+                    width={50}
+                    height="auto"
+                    className="rounded-md pr-2">
+                </Image >
+
+                <h1 className="text-3xl font-bold bg-white p-2 rounded-lg text-black">
+                    WishList
+                </h1>
+            </section>
+
+            {/* Header-Right-Section =  Account details + sign out / Sign in */}
+            <section className="">
+                {/* Placeholder below */}
+                {/* <UserInfo /> */}
+            </section>
+        </section>
+    );
+}
